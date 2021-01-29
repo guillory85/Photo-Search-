80d2b1db579bad478eb9cb19b5806c0070f2bf69
guillory85 <69327186+guillory85@users.noreply.github.com>
2021-01-29 09:00:39 -0600

    html and JS
    made some changes to the pages in html and added away to change pages in JS

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-async function searchPic() {
+async function searchPic(page) {
     let clientId = 'iIaBH2QMP4Qx92SE6q--oAk8s-JZv360YAa5jGax8_4';
     let query = document.getElementById('get').value;
-    let url = 'https://api.unsplash.com/search/photos?per_page=30&query=' + query + '&client_id=' + clientId;
+    let url = 'https://api.unsplash.com/search/photos?per_page=30&query=' + query + '&client_id=' + clientId + '&page=' + page;
 
     const response = await fetch(url);
 
     const item = await response.json();
-
-    console.log(url);
 
     return item;
 }
 
 document.querySelector('#btn').addEventListener('click', async () => {
    
-        let item = await searchPic();
-        item = item.results;
+    let item = await searchPic();
+    let pageTotal = item.total_pages;
+       item = item.results; 
 
-        let output = `<h2>Searh Results</h2>`;
-
+    let output = `<h2>Searh Results</h2>`;
         item.forEach(photo => {
-            let image = photo.urls.thumb;
+          let image = photo.urls.small;
+          let id = photo.id;
             
-           output += `<img src ="${image}"/>`;
+           output += `
+           <div class = "imgStyle">
+            <img src ="${image}"/>
+            <p>${id}</p>
+           </div>
+           `;
             document.querySelector('#output').innerHTML = output;
-
         });
+
+        let showPage = document.querySelector('#pageD');
+        showPage.style.display = 'block';
 });
+
+for (let i = 0; i < document.querySelectorAll('.pageN').length; i++) {
+    document.querySelectorAll('.pageN')[i].addEventListener('click', async () => {
+        let pageT = document.querySelectorAll('.pageN')[i].innerText; 
+        let item = await searchPic(pageT);
+        item = item.results;
+        
+        let output = `<h2>Searh Results</h2>`;
+        item.forEach(photo => {
+          let image = photo.urls.thumb;
+          let id = photo.id;
+            
+           output += `
+           <div>
+            <img src ="${image}"/>
+            <p>${id}</p>
+           </div>
+           `;
+            document.querySelector('#output').innerHTML = output;
+        });
+    });
+}
