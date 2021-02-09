14fe054924d9470521d99954c11d5d4d3a2abbca
guillory85 <69327186+guillory85@users.noreply.github.com>
2021-02-09 13:24:08 -0600

    Add files via upload
    Made some changes to CSS and added try/catch to JS for errors

--- index.js
+++ index.js
@@ -2,36 +2,48 @@ async function searchPic(page) {
     let clientId = 'iIaBH2QMP4Qx92SE6q--oAk8s-JZv360YAa5jGax8_4';
     let query = document.getElementById('get').value;
     let url = 'https://api.unsplash.com/search/photos?per_page=30&query=' + query + '&client_id=' + clientId + '&page=' + page;
-
+    
     const response = await fetch(url);
 
     const item = await response.json();
+    console.log(item);
 
     return item;
 }
 
 document.querySelector('#btn').addEventListener('click', async () => {
-   
-    let item = await searchPic();
-    let pageTotal = item.total_pages;
-       item = item.results; 
+    try {
+        let item = await searchPic();
+        if (!item.results.length) {
+            throw new Error("Could not find what you are looking for!");
+        } else {
+            item = item.results; 
+    
+            let output = `<h2>Searh Results</h2>`;
+                item.forEach(photo => {
+                  let image = photo.urls.thumb;
+                  let description = photo.alt_description;
+                    
+                   output += `
+                   <div class = "imgStyle">
+                    <img src ="${image}" alt = "${description}"/>
+                   </div>
+                   `;
+                    document.querySelector('#output').innerHTML = output;
+                });
+        
+                let showPage = document.querySelector('#pageD');
+                showPage.style.display = 'block';
+        }
 
-    let output = `<h2>Searh Results</h2>`;
-        item.forEach(photo => {
-          let image = photo.urls.thumb;
-          let description = photo.alt_description;
-            
-           output += `
-           <div class = "imgStyle">
-            <img src ="${image}"/>
-            <p>${description}</p>
-           </div>
-           `;
-            document.querySelector('#output').innerHTML = output;
-        });
-
-        let showPage = document.querySelector('#pageD');
-        showPage.style.display = 'block';
+    } catch (error) {
+        let err = error.message;
+        let errorOutput = `<h2>Error</h2>`;
+        errorOutput += `<p>"${err}"</p>`;
+        
+        document.querySelectorAll('.error')[0].innerHTML = errorOutput;
+        console.log(err);
+    }
 });
 
 for (let i = 0; i < document.querySelectorAll('.pageN').length; i++) {
@@ -47,8 +59,7 @@ for (let i = 0; i < document.querySelectorAll('.pageN').length; i++) {
             
            output += `
            <div class = "imgStyle">
-            <img src ="${image}"/>
-            <p>${description}</p>
+            <img src ="${image}" alt = "${description}"/>
            </div>
            `;
             document.querySelector('#output').innerHTML = output;
