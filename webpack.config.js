007b32b78e478de7b531e8be987b88dc9b329efa
unknown <guillory1985@gmail.comgit config --global user.name Terry>
2021-03-01 12:34:52 -0600

    1st commit with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: ['babel-polyfill', './public/src/index.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader',
+                options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-arrow-functions']
+                }
+            }
+        }]
+    }
+}
